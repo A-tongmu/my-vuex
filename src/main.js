0a588cb9ed18b517f9733a836c42a2cfcde46782
王皓 <1109428745@qq.com>
2020-09-30 09:37:52 +0800

    引入公共样式

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/common.less'
 
 if(process.env.NODE_ENV == 'development'){
   require('./mock') 
